85af02220893e7df2444b2fe10619b788fddaa93
bbabb <bbabb@B450IAORUSPROWIFI77d641bf.localdomain>
2022-06-10 23:12:48 -0500

    updates

--- slices/contains.go
+++ slices/contains.go
@@ -1,16 +1,16 @@
 package slices
 
 func Contains[T comparable](xs []T, v T) bool {
-	fn := func(x, y T) bool {
-		return x == y
+	fn := func(x T) bool {
+		return x == v
 	}
 
-	return ContainsFn(xs, v, fn)
+	return ContainsFn(xs, fn)
 }
 
-func ContainsFn[T comparable](xs []T, v T, fn func(T, T) bool) bool {
+func ContainsFn[T comparable](xs []T, fn func(T) bool) bool {
 	for _, x := range xs {
-		if fn(x, v) {
+		if fn(x) {
 			return true
 		}
 	}
