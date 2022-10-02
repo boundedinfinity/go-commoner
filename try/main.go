69114e3b901c717d9ecff0be8597d044c0bfdf22
bbabb <brad.babb@boundedinfinity.com>
2022-10-02 15:30:45 -0500

    imported from https://github.com/boundedinfinity/go-trier

---
+++ try/main.go
@@ -0,0 +1,43 @@
+package try
+
+import (
+	"fmt"
+
+	"github.com/boundedinfinity/go-commoner/errutil"
+)
+
+type Try[T any] struct {
+	Result T     `json:"result"`
+	Error  error `json:"error"`
+}
+
+func (t Try[T]) Failure() bool {
+	return t.Error != nil
+}
+
+func (t Try[T]) Success() bool {
+	return !t.Failure()
+}
+
+func Success[T any](result T) Try[T] {
+	return Try[T]{
+		Result: result,
+	}
+}
+
+func Failure[T any](err error) Try[T] {
+	var zero T
+	return Complete(zero, err)
+}
+
+func Failuref[T any](format string, a ...any) Try[T] {
+	return Failure[T](fmt.Errorf(format, a...))
+}
+
+func Complete[T any](result T, err error) Try[T] {
+	err2 := errutil.Err(err)
+	return Try[T]{
+		Result: result,
+		Error:  &err2,
+	}
+}
