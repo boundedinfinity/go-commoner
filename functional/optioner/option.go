e3807adaff16dd1538df805253c45b7b0ae862ec
Brad Babb <brad_babb@cable.comcast.com>
2023-10-12 22:43:27 -0500

    updates

--- functional/optioner/option.go
+++ functional/optioner/option.go
@@ -40,12 +40,3 @@ func (t Option[T]) OrElse(v T) T {
 
 	return v
 }
-
-func (t Option[T]) Then(fn func(T) T) Option[T] {
-	if t.Defined() {
-		v := fn(t.Get())
-		return Some(v)
-	}
-
-	return None[T]()
-}
