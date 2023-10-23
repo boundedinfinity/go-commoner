ef1b565e51d730f910982457b38d3336c8134f2b
Brad Babb <brad_babb@cable.comcast.com>
2023-10-23 00:45:22 -0500

    v1.0.25

--- idiomatic/slicer/join.go
+++ idiomatic/slicer/join.go
@@ -5,17 +5,17 @@ import (
 	gstring "strings"
 )
 
-func Join[T any](items []T, sep string) string {
-	return JoinFn(items, sep, func(el T) string {
+func Join[T any](sep string, items ...T) string {
+	return JoinFn(sep, func(el T) string {
 		return fmt.Sprintf("%v", el)
-	})
+	}, items...)
 }
 
-func JoinFn[T any](items []T, sep string, fn func(T) string) string {
+func JoinFn[T any](sep string, fn func(T) string, items ...T) string {
 	return gstring.Join(Map(fn, items...), sep)
 }
 
-func JoinErrFn[T any](items []T, sep string, fn func(T) (string, error)) (string, error) {
+func JoinErrFn[T any](sep string, fn func(T) (string, error), items ...T) (string, error) {
 	res, err := MapErr(fn, items...)
 
 	if err != nil {
--- idiomatic/slicer/join_test.go
+++ idiomatic/slicer/join_test.go
@@ -10,7 +10,7 @@ import (
 func Test_Join(t *testing.T) {
 	expected := "1,2,3"
 	input := []int{1, 2, 3}
-	actual := slicer.Join(input, ",")
+	actual := slicer.Join(",", input...)
 
 	assert.Equal(t, expected, actual)
 }
