d2568c4df7b775340c4abc7a5dac6211ff1a8a5b
bbabb <brad.babb@boundedinfinity.com>
2023-12-06 23:08:05 -0600

    updates

---
+++ idiomatic/stringer/lang.go
@@ -0,0 +1,28 @@
+package stringer
+
+import (
+	"github.com/boundedinfinity/go-commoner/idiomatic/utf"
+)
+
+var (
+	languageCharacters = []utf.UtfChar{}
+)
+
+func init() {
+	languageCharacters = append(languageCharacters, utf.Utf7.Letters()...)
+	languageCharacters = append(languageCharacters, utf.UNDERSCORE)
+}
+
+func ReplaceNonLanguageCharacters[T ~string](s T, replacement T, startsWithNumber T) string {
+	o := replaceNotInList(s, utf.Utf7.ToStrings(languageCharacters), string(replacement))
+
+	if len(o) > 0 && utf.Utf7.IsNumber(o[0]) {
+		o = string(startsWithNumber) + o
+	}
+
+	return o
+}
+
+func RemoveNonLanguageCharacters[T ~string](s T, startsWithNumber T) string {
+	return ReplaceNonLanguageCharacters(s, "", startsWithNumber)
+}
