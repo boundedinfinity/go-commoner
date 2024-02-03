eb93d843c0af1863cfc32954610215535ba7ad82
Brad Babb <brad_babb@cable.comcast.com>
2024-02-03 16:39:39 -0600

    updates

--- idiomatic/langer/go.go
+++ idiomatic/langer/go.go
@@ -1,13 +1,25 @@
 package langer
 
+import "errors"
+
 // https://go.dev/ref/spec#Identifiers
 
 var Go = golang{}
 
-type golang struct{}
+type golang struct {
+	keywords                             []string
+	ErrIdentifierInvalidIsKeyword        error
+	ErrIdentifierInvalidStartsWithNumber error
+}
 
 func (t golang) Identifier(s string) (string, error) {
 	o := s
+
+	for _, keyword := range t.keywords {
+		if s == keyword {
+			return o, t.ErrIdentifierInvalidIsKeyword
+		}
+	}
 
 	return o, nil
 }
@@ -20,4 +32,16 @@ func (t golang) MustIdentifier(s string) string {
 	}
 
 	return o
+}
+
+func init() {
+	Go.keywords = []string{
+		"break", "default", "func", "interface", "select", "case", "defer",
+		"go", "map", "struct", "chan", "else", "goto", "package", "switch",
+		"const", "fallthrough", "if", "range", "type", "continue", "for",
+		"import", "return", "var",
+	}
+
+	Go.ErrIdentifierInvalidIsKeyword = errors.New("invalid go identifier: is a keyword")
+	Go.ErrIdentifierInvalidStartsWithNumber = errors.New("invalid go identifier: starts with number")
 }
