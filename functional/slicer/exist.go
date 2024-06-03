a5685f4a1997542a16752b06c31ba913cb356d7a
Brad Babb <brad_babb@cable.comcast.com>
2024-06-03 16:49:07 -0500

    updates

--- functional/slicer/exist.go
+++ functional/slicer/exist.go
@@ -9,10 +9,10 @@ func Exist[T comparable](s T, items ...T) bool {
 	return slicer.Exist(s, items...)
 }
 
-func ExistFn[T any](fn func(T) bool, items ...T) bool {
+func ExistFn[T any](fn func(int, T) bool, items ...T) bool {
 	return slicer.ExistFn(fn, items...)
 }
 
-func ExistFnErr[T any](fn func(T) (bool, error), items ...T) trier.Try[bool] {
+func ExistFnErr[T any](fn func(int, T) (bool, error), items ...T) trier.Try[bool] {
 	return trier.Complete(slicer.ExistFnErr(fn, items...))
 }
