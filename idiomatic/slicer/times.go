4cc5887fa8fa002545c5b85bc18fe13e107c6eb3
Brad Babb <brad_babb@cable.comcast.com>
2024-06-12 09:29:35 -0500

    updates

--- idiomatic/slicer/times.go
+++ idiomatic/slicer/times.go
@@ -1,11 +1,39 @@
 package slicer
 
-func Times[V any, O any](v V, s int, fn func(V) O) []O {
-	var os []O
+func Times[T any](count int, elems ...T) []T {
+	fn := func(_, _ int, t T) T { return t }
+	return TimesFn(fn, count, elems...)
+}
 
-	for i := 0; i < s; i++ {
-		os = append(os, fn(v))
+func TimesFn[T any, U any](fn func(i, j int, t T) U, count int, elems ...T) []U {
+	fn2 := func(i, j int, t T) (U, error) {
+		return fn(i, j, t), nil
 	}
 
-	return os
+	results, _ := TimesFnErr(fn2, count, elems...)
+	return results
+}
+
+func TimesFnErr[T any, U any](fn func(i, j int, t T) (U, error), count int, elems ...T) ([]U, error) {
+	results := []U{}
+	var result U
+	var err error
+
+	for i := 0; i < count; i++ {
+		for j, elem := range elems {
+			result, err = fn(i, j, elem)
+
+			if err != nil {
+				break
+			}
+
+			results = append(results, result)
+		}
+
+		if err != nil {
+			break
+		}
+	}
+
+	return results, err
 }
--- idiomatic/slicer/times_test.go
+++ idiomatic/slicer/times_test.go
@@ -1,1 +1,72 @@
 package slicer_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/boundedinfinity/go-commoner/idiomatic/slicer"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Times(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    []string
+		count    int
+		expected []string
+	}{
+		{
+			name:     "duplicates",
+			input:    []string{"a"},
+			count:    2,
+			expected: []string{"a", "a"},
+		},
+		{
+			name:     "duplicates",
+			input:    []string{"a", "b"},
+			count:    2,
+			expected: []string{"a", "b", "a", "b"},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(tt *testing.T) {
+			actual := slicer.Times(tc.count, tc.input...)
+			assert.Equal(t, tc.expected, actual, tc.name)
+		})
+	}
+}
+
+func Test_TimesFn(t *testing.T) {
+	fn := func(i, j int, s string) string { return fmt.Sprintf("%v:%v:%v", i, j, s) }
+
+	testCases := []struct {
+		name     string
+		input    []string
+		count    int
+		fn       func(int, int, string) string
+		expected []string
+	}{
+		{
+			name:     "duplicates",
+			input:    []string{"a"},
+			count:    2,
+			fn:       fn,
+			expected: []string{"0:0:a", "1:0:a"},
+		},
+		{
+			name:     "duplicates",
+			input:    []string{"a", "b"},
+			count:    2,
+			fn:       fn,
+			expected: []string{"0:0:a", "0:1:b", "1:0:a", "1:1:b"},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(tt *testing.T) {
+			actual := slicer.TimesFn(tc.fn, tc.count, tc.input...)
+			assert.Equal(t, tc.expected, actual, tc.name)
+		})
+	}
+}
