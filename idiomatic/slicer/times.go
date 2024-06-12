23d654907d97d36221071f8dc49171592312edc9
Brad Babb <brad_babb@cable.comcast.com>
2024-06-12 15:11:03 -0500

    updates

--- idiomatic/slicer/times.go
+++ idiomatic/slicer/times.go
@@ -1,39 +1,11 @@
 package slicer
 
 func Times[T any](count int, elems ...T) []T {
-	fn := func(_, _ int, t T) T { return t }
-	return TimesFn(fn, count, elems...)
-}
-
-func TimesFn[T any, U any](fn func(i, j int, t T) U, count int, elems ...T) []U {
-	fn2 := func(i, j int, t T) (U, error) {
-		return fn(i, j, t), nil
-	}
-
-	results, _ := TimesFnErr(fn2, count, elems...)
-	return results
-}
-
-func TimesFnErr[T any, U any](fn func(i, j int, t T) (U, error), count int, elems ...T) ([]U, error) {
-	results := []U{}
-	var result U
-	var err error
+	results := []T{}
 
 	for i := 0; i < count; i++ {
-		for j, elem := range elems {
-			result, err = fn(i, j, elem)
-
-			if err != nil {
-				break
-			}
-
-			results = append(results, result)
-		}
-
-		if err != nil {
-			break
-		}
+		results = append(results, elems...)
 	}
 
-	return results, err
+	return results
 }
--- idiomatic/slicer/times_test.go
+++ idiomatic/slicer/times_test.go
@@ -1,7 +1,6 @@
 package slicer_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/boundedinfinity/go-commoner/idiomatic/slicer"
@@ -32,40 +31,6 @@ func Test_Times(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(tt *testing.T) {
 			actual := slicer.Times(tc.count, tc.input...)
-			assert.Equal(t, tc.expected, actual, tc.name)
-		})
-	}
-}
-
-func Test_TimesFn(t *testing.T) {
-	fn := func(i, j int, s string) string { return fmt.Sprintf("%v:%v:%v", i, j, s) }
-
-	testCases := []struct {
-		name     string
-		input    []string
-		count    int
-		fn       func(int, int, string) string
-		expected []string
-	}{
-		{
-			name:     "duplicates",
-			input:    []string{"a"},
-			count:    2,
-			fn:       fn,
-			expected: []string{"0:0:a", "1:0:a"},
-		},
-		{
-			name:     "duplicates",
-			input:    []string{"a", "b"},
-			count:    2,
-			fn:       fn,
-			expected: []string{"0:0:a", "0:1:b", "1:0:a", "1:1:b"},
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(tt *testing.T) {
-			actual := slicer.TimesFn(tc.fn, tc.count, tc.input...)
 			assert.Equal(t, tc.expected, actual, tc.name)
 		})
 	}
