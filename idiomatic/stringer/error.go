1d63f456836c96336dc399c7d2703093607f27ad
Brad Babb <brad_babb@cable.comcast.com>
2026-04-17 15:48:12 -0500

    updates

---
+++ idiomatic/stringer/error.go
@@ -0,0 +1,5 @@
+package stringer
+
+import "github.com/boundedinfinity/go-commoner/idiomatic/errorer"
+
+var ErrStringer = errorer.New("stringer error")
--- idiomatic/stringer/transform.go
+++ idiomatic/stringer/transform.go
@@ -1,5 +1,7 @@
 package stringer
 
+import "github.com/boundedinfinity/go-commoner/idiomatic/errorer"
+
 func TarnsformFunc[T ~string](s T, fns ...func(string) string) string {
 	native := string(s)
 
@@ -20,4 +22,49 @@ func TransformAllFunc[T ~string](items []T, fns ...func(string) string) []string
 	}
 
 	return results
+}
+
+var (
+	ErrTransform   = errorer.New("transform error")
+	errTransformFn = errorer.Func(ErrStringer, ErrTransform)
+)
+
+func TransformFuncErr[T ~string](s T, fns ...func(string) (string, error)) (string, error) {
+	native := string(s)
+	var err error
+
+	for i, fn := range fns {
+		if fn != nil {
+			if native, err = fn(native); err != nil {
+				err = errTransformFn("fns[%d] : %w", i, err)
+				break
+			}
+		}
+	}
+
+	return native, err
+}
+
+func TransformAllFuncErr[T ~string](items []T, fns ...func(string) (string, error)) ([]string, error) {
+	results := make([]string, len(items))
+	var err error
+
+	for i := range items {
+		if err != nil {
+			break
+		}
+
+		native := string(items[i])
+
+		for j, fn := range fns {
+			if fn != nil {
+				if native, err = fn(native); err != nil {
+					err = errTransformFn("item[%d] : fns[%d] : %w", i, j, err)
+					break
+				}
+			}
+		}
+	}
+
+	return results, err
 }
