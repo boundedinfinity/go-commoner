4ffb2f6212f1e680be10bfa97b0012889e4decd3
Brad Babb <brad_babb@cable.comcast.com>
2026-04-29 08:50:28 -0500

    updates

--- idiomatic/stringer/chain.go
+++ idiomatic/stringer/chain.go
@@ -7,3 +7,16 @@ func Chain[S ~string](s S, fns ...func(S) S) S {
 
 	return s
 }
+
+func ChainErr[S ~string](s S, fns ...func(S) (S, error)) (S, error) {
+	var err error
+
+	for _, fn := range fns {
+		s, err = fn(s)
+		if err != nil {
+			return s, err
+		}
+	}
+
+	return s, nil
+}
